4bc30071204a829184be23875939d613105e534c
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2023-03-17 16:56:00 +0900

    (feat: weather-mid) URLConnection으로 중기 날씨 받기 & EnvLoader 클래스 추가

--- src/main/java/com/weatherwhere/weatherservice/WeatherserviceApplication.java
+++ src/main/java/com/weatherwhere/weatherservice/WeatherserviceApplication.java
@@ -1,13 +1,16 @@
 package com.weatherwhere.weatherservice;
 
+import com.weatherwhere.weatherservice.config.EnvLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(EnvLoader.class)
 public class WeatherserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeatherserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherserviceApplication.class, args);
+    }
 
 }
