4232d3df1eb1a7a77bec32b1e8bc3d52a79d9632
j2woo <kjk04021@naver.com>
2023-04-08 19:48:29 +0900

    (refactor: edit tourApiService) 해당 날짜에 해당하는 날씨 중기 예보 데이터 전부 조회하기

--- src/main/java/com/weatherwhere/weatherservice/repository/weathermid/WeatherMidRepository.java
+++ src/main/java/com/weatherwhere/weatherservice/repository/weathermid/WeatherMidRepository.java
@@ -4,8 +4,10 @@ import com.weatherwhere.weatherservice.domain.weathermid.WeatherMidCompositeKey;
 import com.weatherwhere.weatherservice.domain.weathermid.WeatherMidEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface WeatherMidRepository extends JpaRepository<WeatherMidEntity, WeatherMidCompositeKey> {
     Optional<WeatherMidEntity> findById(WeatherMidCompositeKey weatherMidCompositeKey);
+    List<WeatherMidEntity> findByIdBaseTime(String baseTime);
 }
