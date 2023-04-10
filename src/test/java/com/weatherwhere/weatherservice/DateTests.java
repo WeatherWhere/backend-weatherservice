8875ad1959832b4f3e89adf6b6d658fe7f607da2
hwang-jubin <117631618+hwang-jubin@users.noreply.github.com>
2023-04-10 23:21:40 +0900

    Revert "(fix: github action test를 위한 test code 주석처리)"
    This reverts commit 4dd4cb617652db4aa0873bc22bd8d075729e99f9.

--- src/test/java/com/weatherwhere/weatherservice/DateTests.java
+++ src/test/java/com/weatherwhere/weatherservice/DateTests.java
@@ -1,27 +1,27 @@
-//package com.weatherwhere.weatherservice;
-//
-//import com.weatherwhere.weatherservice.service.date.DateService;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import java.util.Arrays;
-//
-//@SpringBootTest
-//public class DateTests {
-//    @Autowired
-//    private DateService dateService;
-//
-//    @Test
-//    @DisplayName("오늘 날짜를 기준으로 '첫 번쨰 매개변수'일 후 - '두 번째 매개변수'일 후 까지의 날짜를 리스트로 리턴하는 테스트")
-//    void testGetDaysAfterToday() {
-//        System.out.println(Arrays.toString(dateService.getDaysAfterToday(3, 7)));
-//    }
-//
-//    @Test
-//    @DisplayName("openAPI에 변수로 넘겨줄 적절한 tmfc를 반환하는 메서드")
-//    void testGetTmfc() {
-//        System.out.println(dateService.getTmfc());
-//    }
-//}
+package com.weatherwhere.weatherservice;
+
+import com.weatherwhere.weatherservice.service.date.DateService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+
+@SpringBootTest
+public class DateTests {
+    @Autowired
+    private DateService dateService;
+
+    @Test
+    @DisplayName("오늘 날짜를 기준으로 '첫 번쨰 매개변수'일 후 - '두 번째 매개변수'일 후 까지의 날짜를 리스트로 리턴하는 테스트")
+    void testGetDaysAfterToday() {
+        System.out.println(Arrays.toString(dateService.getDaysAfterToday(3, 7)));
+    }
+
+    @Test
+    @DisplayName("openAPI에 변수로 넘겨줄 적절한 tmfc를 반환하는 메서드")
+    void testGetTmfc() {
+        System.out.println(dateService.getTmfc());
+    }
+}
--- src/test/java/com/weatherwhere/weatherservice/WeatherMidTests.java
+++ src/test/java/com/weatherwhere/weatherservice/WeatherMidTests.java
@@ -1,54 +1,54 @@
-//package com.weatherwhere.weatherservice;
-//
-//import com.weatherwhere.weatherservice.domain.weathermid.WeatherMidCompositeKey;
-//import com.weatherwhere.weatherservice.domain.weathermid.WeatherMidEntity;
-//import com.weatherwhere.weatherservice.service.date.DateService;
-//import com.weatherwhere.weatherservice.service.weathermid.WeatherMidService;
-//import com.weatherwhere.weatherservice.service.weathermid.ParseCSVService;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import java.util.List;
-//
-//@SpringBootTest
-//public class WeatherMidTests {
-//    @Autowired
-//    private WeatherMidService weatherMidService;
-//
-//    @Autowired
-//    private ParseCSVService parseCSVService;
-//
-//    @Autowired
-//    private DateService dateService;
-//
-//    @Test
-//    @DisplayName("중기 기온 예보와 중기 육상 예보 2개의 openAPI를 호출하는 테스트")
-//    void testWeatherOpenAPI() {
-//        try {
-//            System.out.println(weatherMidService.getWeatherMidTa("11111111", dateService.getTmfc()));
-//            System.out.println(weatherMidService.getWeatherMidLandFcst("11H20000", dateService.getTmfc()));
-//        } catch (Exception e) {
-//            System.out.println(e.getLocalizedMessage());
-//            e.printStackTrace();
-//        }
-//    }
-//
-//
-//    @Test
-//    @DisplayName("중기 예보 API 2개를 합쳐 Entity 리스트를 반환하는 메서드를 확인.")
-//    void testGetWeatherMidEntities() {
-//        long start = System.nanoTime();
-//        List<WeatherMidEntity> entities = weatherMidService.makeEntityList(parseCSVService.ParseCSV(),
-//                dateService.getDaysAfterToday(3, 7), dateService.getTmfc());
-//        long openApiEnd = System.nanoTime();
-//        System.out.println("약 172번의 OpenAPI 호출을 통해 850개의 Entity리스트를 만드는데 소요되는시간: " + (openApiEnd - start) + "ns");
-//
-//
-//        List<WeatherMidCompositeKey> keys = weatherMidService.updateWeatherMid(entities);
-//        long dbEnd = System.nanoTime();
-//        System.out.println("약 850개의 Entity리스트를 DB에 업데이트하는데 소요되는 시간: " + (dbEnd - openApiEnd) + "ns");
-//        System.out.println(keys.toString());
-//    }
-//}
+package com.weatherwhere.weatherservice;
+
+import com.weatherwhere.weatherservice.domain.weathermid.WeatherMidCompositeKey;
+import com.weatherwhere.weatherservice.domain.weathermid.WeatherMidEntity;
+import com.weatherwhere.weatherservice.service.date.DateService;
+import com.weatherwhere.weatherservice.service.weathermid.WeatherMidService;
+import com.weatherwhere.weatherservice.service.weathermid.ParseCSVService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class WeatherMidTests {
+    @Autowired
+    private WeatherMidService weatherMidService;
+
+    @Autowired
+    private ParseCSVService parseCSVService;
+
+    @Autowired
+    private DateService dateService;
+
+    @Test
+    @DisplayName("중기 기온 예보와 중기 육상 예보 2개의 openAPI를 호출하는 테스트")
+    void testWeatherOpenAPI() {
+        try {
+            System.out.println(weatherMidService.getWeatherMidTa("11111111", dateService.getTmfc()));
+            System.out.println(weatherMidService.getWeatherMidLandFcst("11H20000", dateService.getTmfc()));
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    @DisplayName("중기 예보 API 2개를 합쳐 Entity 리스트를 반환하는 메서드를 확인.")
+    void testGetWeatherMidEntities() {
+        long start = System.nanoTime();
+        List<WeatherMidEntity> entities = weatherMidService.makeEntityList(parseCSVService.ParseCSV(),
+                dateService.getDaysAfterToday(3, 7), dateService.getTmfc());
+        long openApiEnd = System.nanoTime();
+        System.out.println("약 172번의 OpenAPI 호출을 통해 850개의 Entity리스트를 만드는데 소요되는시간: " + (openApiEnd - start) + "ns");
+
+
+        List<WeatherMidCompositeKey> keys = weatherMidService.updateWeatherMid(entities);
+        long dbEnd = System.nanoTime();
+        System.out.println("약 850개의 Entity리스트를 DB에 업데이트하는데 소요되는 시간: " + (dbEnd - openApiEnd) + "ns");
+        System.out.println(keys.toString());
+    }
+}
