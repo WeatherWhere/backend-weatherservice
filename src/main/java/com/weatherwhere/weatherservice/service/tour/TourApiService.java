34d93f60f01314c533f1d1b22ac0638ec4e33775
it's U <40010878+j2woo@users.noreply.github.com>
2023-04-11 15:04:54 +0900

    (refactor: edit tourApiService) 해당 날짜에 해당하는 날씨 중기 예보 데이터 전부 조회하기 (#35)

--- src/main/java/com/weatherwhere/weatherservice/service/tour/TourApiService.java
+++ src/main/java/com/weatherwhere/weatherservice/service/tour/TourApiService.java
@@ -1,9 +1,11 @@
 package com.weatherwhere.weatherservice.service.tour;
 
+import java.util.List;
+
 import com.weatherwhere.weatherservice.domain.weathermid.WeatherMidEntity;
 import com.weatherwhere.weatherservice.dto.ResultDTO;
 import com.weatherwhere.weatherservice.dto.weathermid.WeatherMidDTO;
 
 public interface TourApiService {
-    ResultDTO<WeatherMidDTO> getWeatherMidData(String regionCode, String baseDate);
+    ResultDTO<List<WeatherMidDTO>> getWeatherMidData(String baseTime);
 }
--- src/main/java/com/weatherwhere/weatherservice/service/tour/TourApiServiceImpl.java
+++ src/main/java/com/weatherwhere/weatherservice/service/tour/TourApiServiceImpl.java
@@ -1,17 +1,17 @@
 package com.weatherwhere.weatherservice.service.tour;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.weatherwhere.weatherservice.domain.weathermid.WeatherMidCompositeKey;
 import com.weatherwhere.weatherservice.domain.weathermid.WeatherMidEntity;
 import com.weatherwhere.weatherservice.dto.ResultDTO;
 import com.weatherwhere.weatherservice.dto.weathermid.WeatherMidDTO;
 import com.weatherwhere.weatherservice.repository.weathermid.WeatherMidRepository;
-import com.weatherwhere.weatherservice.service.tour.TourApiService;
 import com.weatherwhere.weatherservice.service.weathermid.WeatherMidService;
 
 import lombok.RequiredArgsConstructor;
@@ -23,27 +23,28 @@ import lombok.extern.log4j.Log4j2;
 public class TourApiServiceImpl implements TourApiService {
     private final WeatherMidRepository weatherMidRepository;
     private final WeatherMidService weatherMidService;
+    private List<WeatherMidDTO> entityToDTOList(List<WeatherMidEntity> list) {
+        List<WeatherMidDTO> dtoList = new ArrayList<>();
+        for(WeatherMidEntity weatherMidEntity : list){
+            dtoList.add(weatherMidService.entityToDTO(weatherMidEntity));
+        }
+        return dtoList;
+    }
     @Override
     @Transactional
-    public ResultDTO<WeatherMidDTO> getWeatherMidData(String regionCode, String baseDate) {
-        WeatherMidCompositeKey weatherMidCompositeKey = new WeatherMidCompositeKey(regionCode,baseDate);
+    public ResultDTO<List<WeatherMidDTO>> getWeatherMidData(String baseTime) {
+        List<WeatherMidDTO> list = new ArrayList<>();
         try {
-            WeatherMidEntity weatherMidEntity = weatherMidRepository.findById(weatherMidCompositeKey)
-                .orElseThrow(() -> new NoSuchElementException());
-            WeatherMidDTO data=weatherMidService.entityToDTO(weatherMidEntity);
-
-            log.info("날씨 중기예보: {}",data);
-            return ResultDTO.of(HttpStatus.OK.value(),"날씨 중기예보를 조회하는데 성공하였습니다.",data);
+            List<WeatherMidEntity> weatherMidEntityList = weatherMidRepository.findByIdBaseTime(baseTime);
+            list = entityToDTOList(weatherMidEntityList);
+            log.info("날씨 중기예보: {}",list);
         } catch (NoSuchElementException e) {
             // db에서 찾는 데이터 없을 경우
             e.getStackTrace();
             log.error("db에 날씨 중기예보 데이터 없음");
-            // 수정해야함
-            return null;
         } catch (Exception e) {
             log.error(e.getMessage());
-            // 수정해야함
-            return null;
         }
+        return ResultDTO.of(HttpStatus.OK.value(),"날씨 중기예보를 조회하는데 성공하였습니다.",list);
     }
 }
