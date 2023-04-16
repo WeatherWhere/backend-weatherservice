36770925b814ef66d9d81aa1518cef1e29a5d3b5
Baekys97 <117141532+Baekys97@users.noreply.github.com>
2023-04-16 17:04:01 +0900

    (fix: shortbatch time 변경) (#41)
    1. 조건문 사용하여 클라우드워치에서 돌아가는 시간에 해당하는 발표시간으로 api 호출하게 변경

--- src/main/java/com/weatherwhere/weatherservice/service/date/DateService.java
+++ src/main/java/com/weatherwhere/weatherservice/service/date/DateService.java
@@ -1,7 +1,11 @@
 package com.weatherwhere.weatherservice.service.date;
 
+import com.weatherwhere.weatherservice.dto.weathershort.WeatherShortRequestDTO;
+
 public interface DateService {
     String[] getDaysAfterToday(int start, int end);
 
     String getTmfc();
+
+    WeatherShortRequestDTO getBaseDateTime(WeatherShortRequestDTO weatherShortRequestDTO);
 }
