29869410b8aa3a867fa1538cdc92ea4253fb9b93
Baekys97 <117141532+Baekys97@users.noreply.github.com>
2023-04-18 20:54:37 +0900

    (fix:cors-error) (#45)

--- src/main/java/com/weatherwhere/weatherservice/WeatherserviceApplication.java
+++ src/main/java/com/weatherwhere/weatherservice/WeatherserviceApplication.java
@@ -5,7 +5,10 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.File;
 import java.io.IOException;
@@ -31,4 +34,16 @@ public class WeatherserviceApplication {
         }
         SpringApplication.run(WeatherserviceApplication.class, args);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000","https://dpezzthgwnnvc.cloudfront.net");
+            }
+        };
+    }
+
+
 }
